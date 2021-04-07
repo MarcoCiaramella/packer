6710553d03ce058d5961def63fd8751b858a9b36
Marco <marcociaramella87@gmail.com>
2021-04-07 16:17:13 +0200

    refactoring

--- packer.c
+++ packer.c
@@ -3,7 +3,8 @@
 
 
 byte *alloc_buffer(uint num_vals, byte num_bits){
-    uint size = ceil(num_bits/8.0 * num_vals) + UINT_SIZE - ceil(num_bits/8.0);
+    float num_bytes = num_bits/8.0;
+    uint size = ceil(num_bytes * num_vals) + UINT_SIZE - ceil(num_bytes);
     byte *buffer = (byte *) malloc(size);
     memset(buffer, 0, size);
     return buffer;
@@ -18,8 +19,9 @@ byte *pack(uint *vals, uint size, byte num_bits){
     int i;
     for (i = 0; i < size; i++){
         uint val = vals[i] << (UINT_SIZE - num_bits);
-        uint byte_index = (i * num_bits) / 8;
-        byte byte_offset = (i * num_bits) % 8;
+        uint n = i * num_bits;
+        uint byte_index = n / 8;
+        byte byte_offset = n % 8;
         *to_uint(buffer, byte_index) |= val >> byte_offset;
     }
     return buffer;
@@ -29,8 +31,9 @@ uint *unpack(byte *buffer, uint size, byte num_bits){
     uint *vals = (uint *) malloc(UINT_SIZE/8 * size);
     int i;
     for (i = 0; i < size; i++){
-        uint byte_index = (i * num_bits) / 8;
-        byte byte_offset = (i * num_bits) % 8;
+        uint n = i * num_bits;
+        uint byte_index = n / 8;
+        byte byte_offset = n % 8;
         uint a = *to_uint(buffer, byte_index);
         a <<= byte_offset;
         a >>= UINT_SIZE - num_bits;
